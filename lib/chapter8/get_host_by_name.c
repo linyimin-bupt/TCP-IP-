40228c10615982d2e6f725691a2e26799e95c075
linyimin-bupt <linyimin520812@gmail.com>
2019-05-07 10:39:18 +0800

    add chapter8 example code

---
+++ lib/chapter8/get_host_by_name.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+void error_handling(char *message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Usage: %s <address>", argv[0]);
+        exit(1);
+    }
+
+    struct hostent *host = gethostbyname(argv[1]);
+
+    if (host == NULL) {
+        error_handling("gethostbyname() error");
+    }
+
+    printf("name: %s\n", host->h_name);
+    printf("alias: \n");
+    for (int i = 0; host->h_aliases[i]; i++) {
+        printf("%s ", host->h_aliases[i]);
+    }
+
+    printf("\n");
+
+    printf("address type: %s\n", host->h_addrtype == AF_INET ? "AF_INET" : "Other");
+    printf("adress length; %d\n", host->h_length);
+
+    printf("address list:\n");
+    for (int i = 0; host->h_addr_list[i]; i++) {
+        printf("%s ", inet_ntoa(*(struct in_addr*)host->h_addr_list[i]));
+    }
+
+    printf("\n");
+    return 0;
+}
