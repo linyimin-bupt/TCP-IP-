f9c761b7fb72f55b4edcaa47c974815f12ee1141
linyimin-bupt <linyimin520812@gmail.com>
2019-05-10 21:42:17 +0800

    add chapter11 example code

---
+++ lib/chapter11/pipe.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUF_SIZE 30
+int main(void) {
+    int fds[2];
+    char str[] = "Who are you?";
+    char buf[BUF_SIZE];
+
+    pipe(fds);
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        read(fds[..0], buf, BUF_SIZE);
+        write(fds[1], str, strlen(str));
+    } else {
+        read(fds[0], buf, BUF_SIZE);
+        printf("Received message: %s\n", buf);
+    }
+
+    return 0;
+}
